5c800df0f3230be97a95217017e51b101647fe19
LF0007rlk <luiz.souza.brito@aluno.edu.pi.gov.br>
2025-08-20 08:33:53 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+       double nota = 10;
+   double nota2 = 6;
+   double nota3 = 3;
+   double nota4 = 7;
+   double nota5 = 8;
+   double nota6 = 9;
+   double nota7 = 10;
+   double nota8 = 9;
+  double bimestre1 = (nota + nota2 )/2;
+  System.out.println("A nota do primeiro bimestre é:"+bimestre1);
+   double bimestre2 = (nota3 + nota4)/2;
+  System.out.println("A nota do segundo bimestre é:"+bimestre2);
+   double simestre = (nota + nota2 + nota3 + nota4)/4;
+   System.out.println("A nota do semestre é:"+simestre);
+   double semestre2 = (nota5 + nota6 + nota7 + nota8)/4;
+   System.out.println("A nota do semestre é:"+semestre2);
+   double mediafinal = (simestre + semestre2)/2;
+   System.out.println("A nota media final é:"+mediafinal);
+ System.out.println ("luiz fernando amor da pietra");
+  }
+}
